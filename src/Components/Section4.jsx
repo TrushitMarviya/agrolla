5388357539421d8f7f04dad34c036ac98e5adeec
TrushitMarviya <marviyatrushit0@gmail.com>
2025-05-07 19:00:57 +0530

    Add Section4.css for layout styling with flexbox and responsive design

--- src/Components/Section4.jsx
+++ src/Components/Section4.jsx
@@ -1,6 +1,13 @@
-
+import '@/CSS/Section4.css'
 export default function Section4() {
   return (
-    <div>Section4</div>
+    <div className='main-contain'>
+        <div className="sec1">
+        <img src="/about-agrolla.png" alt="" className="image" />
+        <div className="txt">
+            <h1>About <span style={{color:"#2E7D32"}}>Agrolla</span></h1>
+        </div>
+        </div>
+    </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from  '@/Components/HeroSection'
 import Section3 from '@/Components/Section3';
+import Section4 from '@/Components/Section4';
 export default function Home() {
   return (
     <main>
       <HeroSection/>
       <Section3/>
+      <Section4/>
     </main>
   );
 }
