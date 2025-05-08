d60fdb213a1b4695d8d5a63af3eeebbe4b2ee048
TrushitMarviya <marviyatrushit0@gmail.com>
2025-05-08 12:15:39 +0530

    Add Section6 component and associated CSS for gallery display
    - Created Section6.jsx component to showcase a gallery with a title and description.
    - Added Section6.css for styling the gallery layout, titles, and images.
    - Implemented responsive design with flexbox for image alignment.
    - Included placeholder for images in the gallery section.

---
+++ src/Components/Section6.jsx
@@ -0,0 +1,34 @@
+import "@/CSS/Section6.css";
+export default function Section6() {
+  return (
+    <div className="section2">
+      <center>
+        <div className="title">
+          <hr className="line" />
+          <h2 className="title-text">OUR GALLERY</h2>
+          <hr className="line" />
+        </div>
+      </center>
+      <h1>
+      Capturing Agricultural<span style={{ color: "#2E7D32" }}>Excellence</span> 
+      </h1>
+      <h5>
+      Experience the beauty and innovation of modern agriculture through our lens. Each
+      image tells a story of dedication, technology, and sustainable farming practices.
+      </h5>
+    <div className="gallery1">
+        <div className="sece1">
+            <img src="/gal1.png" alt="" />
+        </div>
+        <div className="sece2"></div>
+        <div className="sece3"></div>
+    </div>
+
+
+
+
+
+
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import HeroSection from  '@/Components/HeroSection'
 import Section3 from '@/Components/Section3';
 import Section4 from '@/Components/Section4';
 import Section5 from '@/Components/Section5';
+import Section6 from '@/Components/Section6';
 export default function Home() {
   return (
     <main>
@@ -9,6 +10,7 @@ export default function Home() {
       <Section3/>
       <Section4/>
       <Section5/> 
+      <Section6/>
     </main>
   );
 }
