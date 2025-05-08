e0c4ece8e3e79ea7da3a4171fe02f8c781a70f17
TrushitMarviya <marviyatrushit0@gmail.com>
2025-05-08 11:47:56 +0530

    Add Section 5 with styling and certification images
    - Created Section5 component to display certification information.
    - Added CSS styles for Section 5 layout and design.
    - Included certification images in the component.

---
+++ src/Components/Section5.jsx
@@ -0,0 +1,31 @@
+import "@/CSS/Section5.css";
+
+export default function Section5() {
+  return (
+    <div className="section1">
+      <center>
+        <div className="title">
+          <hr className="line" />
+          <h2 className="title-text">OUR CERTIFICATION</h2>
+          <hr className="line" />
+        </div>
+      </center>
+      <h1>
+        Quality <span style={{ color: "#2E7D32" }}>Certifications</span> &
+        Standards
+      </h1>
+      <h5>
+        Our commitment to excellence is backed by international certifications
+        and quality standards, ensuring the highest level of product quality and
+        safety.
+      </h5>
+      <div className="company">
+        <img src="/cer1.png" alt=""  className="cer"/>
+        <img src="/cer2.png" alt=""  className="cer"/>
+        <img src="/cer3.png" alt=""  className="cer"/>
+        <img src="/cer4.png" alt=""  className="cer"/>
+        <img src="/cer5.png" alt=""  className="cer"/>
+      </div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import HeroSection from  '@/Components/HeroSection'
 import Section3 from '@/Components/Section3';
 import Section4 from '@/Components/Section4';
+import Section5 from '@/Components/Section5';
 export default function Home() {
   return (
     <main>
       <HeroSection/>
       <Section3/>
       <Section4/>
+      <Section5/> 
     </main>
   );
 }
